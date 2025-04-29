c4ab16dcc6c1b958c95c8519ac93e1835778ce03
Rahul Parmar <rahul.p@auberginesolutions.com>
2025-04-29 20:40:51 +0530

    Troubleshoot 15

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get('/api/test', (req: Request, res: Response) => {
   res.json({
     success: true,
-    message: 'Checking the CI/CD pipeline Probably final time',
+    message: 'Checking the CI/CD pipeline Fifth time',
     timestamp: new Date().toISOString()
   });
 });
